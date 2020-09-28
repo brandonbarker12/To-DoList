420d15dc82800b9278d39888f4ef39003084c4be
brandonbarker12 <brandonjbarker12@gmail.com>
2020-09-28 02:09:51 +0000

    add posts

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 //require express
 var express = require('express');
 
+//require body-parser
+var bodyParser = require('body-parser');
+
 //create express object, call express
 var app = express();
 
 //tells app to use ejs for templates
 app.set('view engine', 'ejs');
 
+//tell app to use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//couple of items todo
+var tasks = ["go to class", "take out the trash"];
+
+//completed items
+var completed = ["extra work"];
+
 //get home page /
 app.get('/', function(req, res){
     //return something to home page
-    res.send('Hello World');
+    res.render('index', {tasks: tasks}); //add completed variable to ejs
+});
+
+//add post method /addtask
+app.post('/addtask', function(req, res){
+    var newTask = req.body.newtask;
+    tasks.push(newTask);
+    //return index
+    res.redirect('/');
+});
+
+//remove post method /removetask
+app.post('/removetask', function(req, res){
+    var removeTask = req.body.check;
+    //push to completed
+    if(typeof removeTask === 'string'){
+        tasks.splice(tasks.indexOf(removeTask), 1);
+    }else if(typeof removeTask === 'object'){
+        for (var i = 0; i < removeTask.length; i++){
+            tasks.splice(tasks.indexOf(removeTask[i]), 1);
+        }
+    }
+    res.redirect('/');
 });
 
 //server setup
